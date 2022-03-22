3e29fdf87bd2dca243c6e1f8d57c03f98d404f57
jehad_naser <jnaser@dg-cash.com>
2022-03-22 12:17:46 +0300

    update version

--- app/src/main/java/com/example/dctoaster/MainActivity.kt
+++ app/src/main/java/com/example/dctoaster/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.dctoaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.toast.CustomToast
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        CustomToast.toastMessage(this,"Toast")
+
+
+
     }
+
 }
